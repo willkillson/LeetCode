6e45a9435bf6545a44f78b5f6be70d30d2ee4a1a
Kevin M Wilkinson <willkillson@gmail.com>
2022-08-20 19:47:26 -0700

    python: group anagrams

---
+++ python/arraysAndHashing/group_anagrams.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        # Map hash -> anagram-str
+        myMap = {}
+        
+        for s in strs:
+            sorted_str = sorted(s);
+            sorted_str = "".join(sorted_str);
+            if sorted_str not in myMap:
+                myMap[sorted_str] = [s];
+            else:
+                myMap[sorted_str].append(s);
+        return list(myMap.values());
+        
+strs = ["eat","tea","tan","ate","nat","bat"]
+solution = Solution()
+
+ans = solution.groupAnagrams(strs)
+print(ans)
+# print("hello")
